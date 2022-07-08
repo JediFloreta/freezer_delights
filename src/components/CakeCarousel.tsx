d260ee1f215ce0898745f7fe1cae995d219d66c0
Jedi Floreta <jedi.floreta@symph.co>
2022-07-08 21:58:55 +0800

    Able to retrieve cake carousel items

--- src/components/CakeCarousel.tsx
+++ src/components/CakeCarousel.tsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { client } from '../client'
 
 function CakeCarousel() {
   const [isCakeCarouLoading, setIsCakeCarouLoading] = useState(true)
   const [CakeCarouSlides, setCakeCarouSlides] = useState([])
+
+  const getCakeCarouSlides = useCallback(async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'imageCarousel' })
+      const responseData = response.items
+      console.log(responseData)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    getCakeCarouSlides()
+  }, [getCakeCarouSlides])
 
   return (
     <div>
